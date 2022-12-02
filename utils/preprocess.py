43efd4e02685d064fe12a95ed37af86ef4c302a7
Alvee <42814494+ALvee-611@users.noreply.github.com>
2022-12-02 09:47:35 -0500

    added all helpers

--- utils/preprocess.py
+++ utils/preprocess.py
@@ -12,6 +12,11 @@ from sklearn.base import BaseEstimator, TransformerMixin
 
 from sklearn.model_selection import train_test_split
 
+from sklearn.preprocessing import StandardScaler, OneHotEncoder
+from sklearn.compose import ColumnTransformer
+
+import joblib
+
 # get_data by specifying folder_name and file_name
 def get_data(folder_name, file_name):
     """
@@ -69,3 +74,76 @@ def split_data(df):
     train_y = df["rating"].copy()
     X_train, X_test, y_train, y_test = train_test_split(X_data, train_y, test_size=0.3, random_state=123)
     return X_train, X_test, y_train, y_test
+
+
+# PREPROCESSING PIPELINE
+
+# Standardize the score, bot_score and bot_rating
+Preprocess = ColumnTransformer([
+    ('StandardScaler', StandardScaler(), [2,4,5]),
+    ('oneHotEncoding', OneHotEncoder(), [3])
+])
+
+
+def evaluate_model(test_data,true_labels, model):
+    """
+        evaluate model and return the RMSE of the model
+    """
+    test_X = Preprocess.transform(test_data)
+    predictions = model.predict(test_X)
+    error = abs(predictions - true_labels)
+    rmse = np.sqrt(np.mean(error))
+    return rmse
+
+
+def save_model_or_pipeline(model, model_name, folder):
+    """
+        save_model saves the model in the model folder
+        save_model(model,model_name): ML model Str -> None
+    """
+    model_n = model_name + '.pkl'
+    model_path = os.path.join('..', folder, model_n)
+    joblib.dump(model, model_path)
+
+
+def load_model_or_pipeline(model_name, folder_name):
+    """
+        load_model loads the model named 'model_name'
+    """
+    model_n = model_name + '.pkl'
+    model_path = os.path.join('..',  folder_name, model_n)
+    load_rf = joblib.load(model_path)
+    return load_rf
+
+
+
+def save_submission(submission_name, model, pipeline_name):
+    """
+        save_submission requires 3 inputs: submission_name which is the name of the submission file,
+        model which is the name of the model to be loaded from the models folder to make the predictions and
+        pipeline_name which is the name of the preprocessing pipeline to be load from the pipeline folder
+
+        save_submission: Str Str Str -> Pandas DataFrame
+    """
+
+    sample_submission = get_data('main_data', 'sample_submission.csv')
+    
+    test_data = get_data('main_data', 'test.csv')
+
+    AddBot = load_model_or_pipeline('Add_bot_features', 'pipeline')
+    testing_data = AddBot.transform(test_data)
+    testing_data.drop(columns = 'rating', inplace = True)
+    
+    # Data Pipeline
+    Preprocess = load_model_or_pipeline(pipeline_name, 'pipeline')
+    
+    scaled_testing = Preprocess.transform(testing_data)
+
+    rating = model.predict(scaled_testing)
+    sample_submission['rating'] = rating
+
+    file_name = submission_name + '.csv'
+    prediction_path = os.path.join('../predictions', file_name)
+    sample_submission.to_csv(prediction_path, index = False)
+
+    return sample_submission
